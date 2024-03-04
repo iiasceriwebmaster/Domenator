fe0dba7f90aab0c17edd53f83b43f2ad094f076d
iiasceri <iiasceri@student.42.fr>
2024-03-04 16:46:07 +0200

    Added binding in car reception

--- app/src/main/java/md/webmasterstudio/domenator/CarReceptionActivity.kt
+++ app/src/main/java/md/webmasterstudio/domenator/CarReceptionActivity.kt
@@ -5,12 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import md.webmasterstudio.domenator.databinding.ActivityCarReceptionBinding
 
 class CarReceptionActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityCarReceptionBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_car_reception)
+        binding = ActivityCarReceptionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
